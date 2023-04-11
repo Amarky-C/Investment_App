b9022ce6d30175335aac2aa8dedfb63a94cfe203
Uzoamaka Mbakogu <127051767+Amarky-C@users.noreply.github.com>
2023-04-11 14:35:27 +0100

    Investment Application

---
+++ investmentapp.py
@@ -0,0 +1,29 @@
+#investment application
+#user enters name of the investor
+#user enters the amount to invest
+#application returns 50% profit on capital
+#user enters amount to withdraw
+#user gets a failed transaction if the inputed withdrawal amount is higher than the wallet balance
+#user withdrawal transaction is successful if withdrawal amount is lower than the wallet balance
+#the application deducts a 7% vat from the wallet balance
+
+
+Profit_percentage = 50
+Vat_percentage = 7
+
+
+def investment():
+       name_of_investor = (input('Enter your account name here: '))
+       print('Welcome!', name_of_investor, 'to Cohort 4 Investment application')
+       wallet = int(input("Enter your investment amount here: N "))
+       wallet_balance = wallet * (1 + (Profit_percentage / 100))
+       print(name_of_investor,'Your wallet has been credited with the sum of:','N', wallet_balance,"with a 50% interest on your capital")
+       withdrawal_amount = int(input("Enter amount you want to withdraw: N"))
+       if withdrawal_amount >= wallet_balance :
+          print(name_of_investor," your transaction failed due to insufficient funds")
+         
+       else: 
+           final_wallet_balance = wallet_balance - withdrawal_amount
+           final_balance = final_wallet_balance * (1 - (Vat_percentage /100))
+           print(name_of_investor,"your withdrawal transaction was successful and your account has been debited with:",'N', withdrawal_amount , "and a 7% service charge has been deducted and ",'your final balance is:' ,'N', final_balance)
+investment()
